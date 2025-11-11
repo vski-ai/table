e445e3d258bf94d39fa7f4648dc0d7018a5cd191
Anton Nesterov <anton@demiurg.io>
2025-11-11 18:08:13 +0100

    acessibility: finalize keyboard navigation

---
+++ keyboard/store.ts
@@ -0,0 +1,29 @@
+import { Signal, signal } from "@preact/signals";
+import { Command, TableState } from "@/store/mod.ts";
+import { RowData } from "@/row/types.ts";
+
+type KeyboardStore = {
+  altKey: Signal<boolean>;
+};
+
+declare module "@/store/types.ts" {
+  interface TableState {
+    keyboard: KeyboardStore;
+  }
+}
+
+export function state() {
+  return {
+    keyboard: {
+      altKey: signal(false),
+    },
+  };
+}
+
+export function persist(_: TableState) {
+  return {};
+}
+
+export function mutate<T>(state: TableState, _: Command<T>) {
+  return state;
+}
