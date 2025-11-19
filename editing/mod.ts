15145b84e35f0dac31c2e8294c9e0f3bd72c2dc7
Anton Nesterov <anton@demiurg.io>
2025-11-19 03:14:52 +0100

    [wip] editing

--- editing/mod.ts
+++ editing/mod.ts
@@ -2,6 +2,6 @@ import { ITableModule } from "@/module/types.ts";
 import * as store from "./store.ts";
 
 export const EditingModule: ITableModule = {
-  name: "$editing",
+  name: "editing",
   store,
 };
